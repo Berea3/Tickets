8c75b8237f171061db657f66a4d17d250c27581e
Berendea Andrei <berendeaandrei03@gmail.com>
2024-04-14 00:28:27 +0300

    file upload and storage

--- Front/src/app/components/add-event/add-event.component.ts
+++ Front/src/app/components/add-event/add-event.component.ts
@@ -18,11 +18,40 @@ import {LinkService} from "../link.service";
 export class AddEventComponent {
 
     theatre: Theatre=new Theatre();
+    newFiles: File[]=[];
 
     constructor(private http: HttpClient, private link: LinkService) {}
 
+    onFileAdded(event: any)
+    {
+        console.log(event);
+        for (let i=0;i<event.target.files.length;i++)
+        {
+            if (event.target.files[i].size>10000000)
+            {
+                console.log("files are too big");
+                continue;
+            }
+            this.newFiles.push(event.target.files[i]);
+        }
+    }
+
     onSubmit()
     {
-        this.http.post(this.link.url+"/theatres/create",this.theatre).subscribe();
+        this.http.post(this.link.url+"/theatres/create",this.theatre).subscribe(
+            (response: any)=>{
+                console.log(response);
+                if (response.id==null) console.log("null id");
+                else
+                {
+                    for (let i=0;i<this.newFiles.length;i++)
+                    {
+                        const formData=new FormData();
+                        formData.append('file',this.newFiles[i]);
+                        this.http.post(this.link.url+'/theatres/create/file/'+response.id,formData).subscribe();
+                    }
+                }
+            }
+        );
     }
 }

e97ee3d17ad414f06068ff94e7a4206bc2540cc2
Berendea Andrei <berendeaandrei03@gmail.com>
2024-04-17 17:24:01 +0300

    id generator

---
+++ Back/src/main/java/com/tickets/entities/generator/Generator.java
@@ -0,0 +1,37 @@
+package com.tickets.entities.generator;
+
+import java.time.LocalTime;
+import java.util.Random;
+
+public class Generator {
+
+    public static String generateId()
+    {
+        StringBuilder s=new StringBuilder();
+        for (char i='a';i<='z';i++) s.append(i);
+        for (char i='A';i<='Z';i++) s.append(i);
+        for (char i='0';i<='9';i++) s.append(i);
+        s.append("!@#$%^&*");
+
+        LocalTime localTime=LocalTime.now();
+        long seed=localTime.getSecond()+localTime.getMinute()*60+ localTime.getHour()*60*60;
+
+        Random random=new Random();
+        random.setSeed(seed);
+
+        StringBuilder id=new StringBuilder();
+
+        int x;
+        for (int i=0;i<32;i++)
+        {
+            x=Math.abs(random.nextInt()%s.length());
+            id.append(s.charAt(x));
+        }
+//        String id="";
+//        return id
+//
+//        ;
+
+        return id.toString();
+    }
+}
--- Back/src/main/java/com/tickets/entities/utils/IdGenerator.java
+++
@@ -1,4 +0,0 @@
-package com.tickets.entities.utils;
-
-public class IdGenerator {
-}

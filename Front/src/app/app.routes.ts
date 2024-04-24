e038afe3de864dab15cfd7501dc1642ca99d0ea4
Berendea Andrei <berendeaandrei03@gmail.com>
2024-04-24 19:56:04 +0300

    updates

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {LoginComponent} from "./components/security/login/login.component";
 import {SignUpComponent} from "./components/security/sign-up/sign-up.component";
 import {AddEventComponent} from "./components/add-event/add-event.component";
 import {TheatreViewComponent} from "./components/home/theatre-view/theatre-view.component";
+import {AddTheatreComponent} from "./components/add-event/add-theatre/add-theatre.component";
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'sign-up', component: SignUpComponent },
     { path: 'add-event', component: AddEventComponent },
+    { path: 'add-event/add-theatre', component: AddTheatreComponent },
     { path: 'events/theatres/:id', component: TheatreViewComponent }
 ];
--- Front/src/app/components/add-event/add-event.component.ts
+++ Front/src/app/components/add-event/add-event.component.ts
@@ -4,54 +4,18 @@ import {FormsModule} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {Theatre} from "../events/Theatre";
 import {LinkService} from "../link.service";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-add-event',
   standalone: true,
     imports: [
         HeaderComponent,
-        FormsModule
+        FormsModule,
+        RouterLink
     ],
   templateUrl: './add-event.component.html',
   styleUrl: './add-event.component.css'
 })
 export class AddEventComponent {
-
-    theatre: Theatre=new Theatre();
-    newFiles: File[]=[];
-
-    constructor(private http: HttpClient, private link: LinkService) {}
-
-    onFileAdded(event: any)
-    {
-        console.log(event);
-        for (let i=0;i<event.target.files.length;i++)
-        {
-            if (event.target.files[i].size>10000000)
-            {
-                console.log("files are too big");
-                continue;
-            }
-            this.newFiles.push(event.target.files[i]);
-        }
-    }
-
-    onSubmit()
-    {
-        this.http.post(this.link.url+"/theatres/create",this.theatre).subscribe(
-            (response: any)=>{
-                console.log(response);
-                if (response.id==null) console.log("null id");
-                else
-                {
-                    for (let i=0;i<this.newFiles.length;i++)
-                    {
-                        const formData=new FormData();
-                        formData.append('file',this.newFiles[i]);
-                        this.http.post(this.link.url+'/theatres/create/file/'+response.id,formData).subscribe();
-                    }
-                }
-            }
-        );
-    }
 }

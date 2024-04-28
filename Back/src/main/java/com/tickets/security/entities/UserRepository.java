611c6ea0e694cbb40f5003732c78602a164c11e8
Berendea Andrei <berendeaandrei03@gmail.com>
2024-04-28 23:36:38 +0300

    id

--- Back/src/main/java/com/tickets/security/entities/UserRepository.java
+++ Back/src/main/java/com/tickets/security/entities/UserRepository.java
@@ -4,10 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface UserRepository extends JpaRepository<User,Long> {
 
     @Query(value="select user from User user where user.username = ?1")
     User findByUsername(String username);
+
+    @Query(value = "select user from User user where user.id=?1")
+    List<String> findById(String id);
 
 }

08e2719d7502c2c9cca9583f653dcb384ead31bb
Berendea Andrei <berendeaandrei03@gmail.com>
2024-10-04 22:56:49 +0300

    cod

--- Front/src/app/components/security/sign-up/sign-up.component.ts
+++ Front/src/app/components/security/sign-up/sign-up.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {User} from "../user";
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-sign-up',

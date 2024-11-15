87739d16ebe711572af7c5173f1efb0857328fe6
Berendea Andrei <berendeaandrei03@gmail.com>
2024-11-15 09:11:03 +0200

    name changes

--- Front/src/app/components/security/security.interceptor.ts
+++
@@ -1,6 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const securityInterceptor: HttpInterceptorFn = (request, next) => {
-    request=request.clone({withCredentials:true});
-    return next(request);
-};

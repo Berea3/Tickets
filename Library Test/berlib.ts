0060adf4234cd83845703e4e8088420500e208b4
Berendea Andrei <berendeaandrei03@gmail.com>
2024-11-21 12:07:48 +0200

    library test project

---
+++ Library Test/berlib.ts
@@ -0,0 +1,20 @@
+export const berlibColors = {
+    berlib: {
+        themes: [
+            {
+                light: {
+                    "primary": "blue",
+                    "secondary": "red",
+                    "secondary-content": "#d3e4e5",
+                },
+            },
+            {
+                dark: {
+                    "primary": "blue",
+                    "secondary": "orange",
+                    "secondary-content": "#d3e4e5",
+                },
+            },
+        ],
+    }
+}

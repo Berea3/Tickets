bb954e051707eac66b76e8f5436dbb3bceb20954
Berendea Andrei <berendeaandrei03@gmail.com>
2024-12-21 22:57:48 +0200

    deleted test files

--- Library Test/projects/berea-lib/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of berea-lib
- */
-
-export * from './lib/berea-lib.service';
-export * from './lib/berea-lib.component';
-export * from './lib/btn/btn.component'
-export * from './lib/data-input/checkbox/checkbox.component'
-export * from './lib/theme.service'

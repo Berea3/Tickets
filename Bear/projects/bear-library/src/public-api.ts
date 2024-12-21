45c5dd398cb3d35a38d2c456ce4cf3971f8858ae
Berendea Andrei <berendeaandrei03@gmail.com>
2024-12-21 23:26:48 +0200

    library exports

---
+++ Bear/projects/bear-library/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of bear-library
+ */
+
+export * from './lib/bear-library.service';
+export * from './lib/bear-library.component';
+export * from './lib/btn/btn.component'
+export * from './lib/data-input/checkbox/checkbox.component'
+export * from './lib/theme.service'

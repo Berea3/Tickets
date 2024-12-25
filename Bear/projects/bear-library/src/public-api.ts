fda0aba232c47c780f7ab325fb2de01d943295b4
Berendea Andrei <berendeaandrei03@gmail.com>
2024-12-25 23:14:27 +0200

    exports

--- Bear/projects/bear-library/src/public-api.ts
+++ Bear/projects/bear-library/src/public-api.ts
@@ -5,10 +5,10 @@
 export * from './lib/bear-library.service';
 export * from './lib/bear-library.component';
 
-export * from './lib/btn/btn.component'
+export * from './lib/actions/bear-btn/bear-btn.component'
 
 // Inputs
-export * from './lib/data-input/input-checkbox/input-checkbox.component'
-export * from './lib/data-input/input-text/input-text.component'
+export * from './lib/data-input/bear-input-checkbox/bear-input-checkbox.component'
+export * from './lib/data-input/bear-input-text/bear-input-text.component'
 
 export * from './lib/theme.service'

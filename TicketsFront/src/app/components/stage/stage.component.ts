b939acc1675c0aac153cc1ddee61258204447ba0
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-02 23:34:03 +0300

    first stage iteration

--- TicketsFront/src/app/components/stage/stage.component.ts
+++ TicketsFront/src/app/components/stage/stage.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {NgClass, NgStyle} from "@angular/common";
+import {HeaderComponent} from '../header/header.component';
+import {BearBtnComponent, BearInputCheckboxComponent} from 'bear-library';
+import {HttpClient} from '@angular/common/http';
+import {LinkService} from '../../services/link.service';
+import {Stage} from '../../entities/Stage';
 
 @Component({
   selector: 'app-stage',
   standalone: true,
     imports: [
         FormsModule,
-        NgClass
+        NgClass,
+        HeaderComponent,
+        BearBtnComponent,
+        BearInputCheckboxComponent
     ],
   templateUrl: './stage.component.html',
   styleUrl: './stage.component.css'
@@ -18,6 +26,26 @@ export class StageComponent {
     rows: number=1;
     columns: number=0;
     zig_zag: boolean=false;
+    map: string;
+
+    stage: Stage=new Stage();
+
+    constructor(private http: HttpClient, private link: LinkService) {}
+
+    ngOnInit()
+    {
+        this.map=this.parseString(this.stage.map);
+    }
+
+    parseString(input: string){
+        const lines = input.split('\n'); // Split the input by newline characters
+        return lines.map(line => line.trim().split(/\s+/)); // Split each line by spaces and remove extra whitespace
+    }
+
+    addStage()
+    {
+        this.http.post(this.link.url+"/stages/addStage",this.stage).subscribe();
+    }
 
     addRow()
     {
@@ -32,5 +60,10 @@ export class StageComponent {
     isOdd(index: number)
     {
         return index%2;
+    }
+
+    changeZigZag(isChecked: boolean)
+    {
+        this.zig_zag=isChecked;
     }
 }

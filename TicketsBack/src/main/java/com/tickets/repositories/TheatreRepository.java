d3a91da932da9241b6ef7f2fdf7cb370d8de7fc3
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-15 19:51:28 +0300

    id in string

--- TicketsBack/src/main/java/com/tickets/repositories/TheatreRepository.java
+++ TicketsBack/src/main/java/com/tickets/repositories/TheatreRepository.java
@@ -3,5 +3,5 @@ package com.tickets.repositories;
 import com.tickets.entities.Theatre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TheatreRepository extends JpaRepository<Theatre,Long> {
+public interface TheatreRepository extends JpaRepository<Theatre,String> {
 }

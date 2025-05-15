01bfeb12e9dc57ccfd43a5836de74a7ad012ded5
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-15 19:57:44 +0300

    stage entity

---
+++ TicketsBack/src/main/java/com/tickets/entities/Stage.java
@@ -0,0 +1,70 @@
+package com.tickets.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="stages")
+public class Stage {
+
+    @Id
+    @Column(name="id")
+    private String id;
+
+    private int rowCount;
+    private int columnCount;
+    private boolean zigzag;
+
+    private String matrix;     // yellow orange red green brown blue    A B C D E F   S-space     G-taken
+
+    public Stage() {
+    }
+
+    public Stage(String id, int rowCount, int columnCount, boolean zigzag, String matrix) {
+        this.id = id;
+        this.rowCount = rowCount;
+        this.columnCount = columnCount;
+        this.zigzag = zigzag;
+        this.matrix = matrix;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public int getColumnCount() {
+        return columnCount;
+    }
+
+    public boolean getZigzag() {
+        return zigzag;
+    }
+
+    public String getMatrix() {
+        return matrix;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public void setColumnCount(int columnCount) {
+        this.columnCount = columnCount;
+    }
+
+    public void setZigzag(boolean zigzag) {
+        this.zigzag = zigzag;
+    }
+
+    public void setMatrix(String matrix) {
+        this.matrix = matrix;
+    }
+}

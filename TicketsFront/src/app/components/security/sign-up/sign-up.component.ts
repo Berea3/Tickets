96f65eb422d75efd186814035d3a7c1b73aa0cd4
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-15 19:43:23 +0300

    sign up component

--- TicketsFront/src/app/components/security/sign-up/sign-up.component.ts
+++ TicketsFront/src/app/components/security/sign-up/sign-up.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
-import {User} from "../user";
+import {User} from "../../../entities/User";
 import { HttpClient } from "@angular/common/http";
+import {LinkService} from '../../../services/link.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -16,13 +17,14 @@ export class SignUpComponent {
     user: User=new User();
     role: String;
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private link: LinkService) {
     }
 
     onSignUp()
     {
         console.log("proba");
         // this.user.roles.push(this.role);
-        this.http.post("http://localhost:1201/security/sign-up",this.user).subscribe();
+        this.http.post(this.link.url+"/security/sign-up",this.user).subscribe();
+        // this.http.post("http://localhost:1201/security/sign-up",this.user).subscribe();
     }
 }

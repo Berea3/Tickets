a50ff10bbe784e8884a3e185d7ec9f41b7492c1a
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-15 19:50:31 +0300

    add concert component

---
+++ TicketsFront/src/app/components/add-event/add-concert/add-concert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-concert',
+  imports: [],
+  templateUrl: './add-concert.component.html',
+  styleUrl: './add-concert.component.css'
+})
+export class AddConcertComponent {
+
+}

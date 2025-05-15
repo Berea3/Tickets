c82af6d880c1bf1e785a89c4a41c7fe5426e6918
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-15 19:49:44 +0300

    sport component

---
+++ TicketsFront/src/app/components/add-event/add-sport/add-sport.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-sport',
+  imports: [],
+  templateUrl: './add-sport.component.html',
+  styleUrl: './add-sport.component.css'
+})
+export class AddSportComponent {
+
+}

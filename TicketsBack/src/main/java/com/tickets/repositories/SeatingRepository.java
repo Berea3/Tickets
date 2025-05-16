244a3e3e40e8a1623b7da14883698114f0a18c41
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-16 17:14:57 +0300

    rename

--- TicketsBack/src/main/java/com/tickets/repositories/SeatingRepository.java
+++ TicketsBack/src/main/java/com/tickets/repositories/SeatingRepository.java
@@ -3,5 +3,5 @@ package com.tickets.repositories;
 import com.tickets.entities.Seating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StageRepository extends JpaRepository<Seating,String> {
+public interface SeatingRepository extends JpaRepository<Seating,String> {
 }

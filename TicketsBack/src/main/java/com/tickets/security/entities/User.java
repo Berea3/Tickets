8aadc7f127ff22af80256bb9070505bb529b119c
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-16 17:15:07 +0300

    user entity

--- TicketsBack/src/main/java/com/tickets/security/entities/User.java
+++ TicketsBack/src/main/java/com/tickets/security/entities/User.java
@@ -1,8 +1,10 @@
 package com.tickets.security.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.tickets.entities.Attachment;
-import com.tickets.entities.Theatre;
+import com.tickets.entities.Concert;
+import com.tickets.entities.Movie;
+import com.tickets.entities.Sport;
+import com.tickets.entities.Theater;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -24,40 +26,55 @@ public class User {
     @JsonIgnore
     @OneToMany(mappedBy = "user",
             cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
-    private List<Theatre> theatres;
+    private List<Theater> theaters;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    private List<Concert> concerts;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    private List<Movie> movies;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    private List<Sport> sports;
 
     public User()
     {
 
     }
 
-    public User(String id, String email, String password, String roles, List<Theatre> theatres)
+    public User(String id, String email, String password, String roles, List<Theater> theaters)
     {
         this.id = id;
         this.email = email;
         this.password = password;
         this.roles = roles;
-        this.theatres=theatres;
+        this.theaters = theaters;
     }
 
-    public void addTheatre(Theatre theatre)
+    public void addTheatre(Theater theater)
     {
-        if (this.theatres==null) {this.theatres=new ArrayList<>();}
-        this.theatres.add(theatre);
-        theatre.setUser(this);
+        if (this.theaters ==null) {this.theaters =new ArrayList<>();}
+        this.theaters.add(theater);
+        theater.setUser(this);
     }
 
     public String getId() {return this.id;}
     public String getPassword() {return this.password;}
     public String getEmail() {return this.email;}
     public String getRoles() {return this.roles;}
-    public List<Theatre> getTheatres() {return theatres;}
+    public List<Theater> getTheaters() {return theaters;}
 
     public void setId(String id) {this.id=id;}
     public void setPassword(String password) {this.password=password;}
     public void setEmail(String email) {this.email=email;}
     public void setRoles(String roles) {this.roles=roles;}
-    public void setTheatres(List<Theatre> theatres) {this.theatres = theatres;}
+    public void setTheaters(List<Theater> theaters) {this.theaters = theaters;}
 
     @Override
     public String toString() {

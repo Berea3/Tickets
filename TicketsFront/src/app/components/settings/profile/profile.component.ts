c31b6fc2b85921d411681f8afde2531ec1e5bf10
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-21 18:22:17 +0300

    translation

--- TicketsFront/src/app/components/settings/profile/profile.component.ts
+++ TicketsFront/src/app/components/settings/profile/profile.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from '../../header/header.component';
+import {TranslateService} from '../../../services/translate.service';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+    imports: [
+        HeaderComponent
+    ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+
+    constructor(private translate: TranslateService) {
+    }
+
+    changeLanguage(lang: 'en'|'ro')
+    {
+        this.translate.changeLanguage(lang);
+    }
 
 }
---
+++ TicketsFront/src/app/services/translate.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as enJson from '../translations/en.json';
+import * as roJson from '../translations/ro.json';
+import {Translation} from '../translations/Translation';
+import {TranslateService} from './translate.service';
+
+@Pipe({
+  name: 'translate',
+    pure: false
+})
+export class TranslatePipe implements PipeTransform {
+
+    private translations={
+        'en': enJson as Translation,
+        'ro': roJson as Translation
+    }
+
+    constructor(private translate: TranslateService) {
+    }
+
+    transform(value: string): any {
+        console.log(this.translations['ro']['Home']);
+        if (value && this.translations[this.translate.getLanguage()] && this.translations[this.translate.getLanguage()][value]) return this.translations[this.translate.getLanguage()][value];
+        return value;
+    }
+
+}

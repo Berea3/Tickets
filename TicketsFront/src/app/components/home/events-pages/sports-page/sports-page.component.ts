84006ba54cdbc1aadbdf3f09105a13cd6d0c59ca
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-22 21:59:08 +0300

    sports

---
+++ TicketsFront/src/app/components/home/events-pages/sports-page/sports-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sports-page',
+  imports: [],
+  templateUrl: './sports-page.component.html',
+  styleUrl: './sports-page.component.css'
+})
+export class SportsPageComponent {
+
+}

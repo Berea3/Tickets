04de5706e31081a472ee3bea7f21f9b908c95e4e
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-22 20:18:59 +0300

    string id

--- TicketsBack/src/main/java/com/tickets/repositories/AttachmentRepository.java
+++ TicketsBack/src/main/java/com/tickets/repositories/AttachmentRepository.java
@@ -3,5 +3,5 @@ package com.tickets.repositories;
 import com.tickets.entities.Attachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AttachmentRepository extends JpaRepository<Attachment,Long> {
+public interface AttachmentRepository extends JpaRepository<Attachment,String> {
 }

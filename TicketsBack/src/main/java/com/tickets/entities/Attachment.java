246d67d751f091a8e3c9ceab0d44262d86462495
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-22 20:18:47 +0300

    attachment entity

--- TicketsBack/src/main/java/com/tickets/entities/Attachment.java
+++ TicketsBack/src/main/java/com/tickets/entities/Attachment.java
@@ -11,16 +11,6 @@ public class Attachment {
     @Column(name="id")
     private String id;
 
-//    @JsonIgnore
-//    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
-//    @JoinColumn(name="theater_id", referencedColumnName = "id")
-//    private Theater theater;
-
-    @JsonIgnore
-    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
-    @JoinColumn(name="concert_id", referencedColumnName = "id")
-    private Concert concert;
-
     private String name;
     private String type;
 
@@ -30,23 +20,20 @@ public class Attachment {
 
     public Attachment () {}
 
-    public Attachment(String id, Concert concert, String name, String type, byte[] file)
+    public Attachment(String id, String name, String type, byte[] file)
     {
         this.id = id;
-        this.concert = concert;
         this.name = name;
         this.type = type;
         this.file = file;
     }
 
     public String getId() {return id;}
-    public Concert getConcert() {return concert;}
     public String getName() {return name;}
     public String getType() {return type;}
     public byte[] getFile() {return file;}
 
     public void setId(String id) {this.id = id;}
-    public void setConcert(Concert concert) {this.concert = concert;}
     public void setName(String name) {this.name = name;}
     public void setType(String type) {this.type = type;}
     public void setFile(byte[] file) {this.file = file;}

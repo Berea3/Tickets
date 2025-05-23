3ee7a370f71def59f2ffcafa7f480f720c0e3258
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-23 21:45:10 +0300

    movies page

--- TicketsFront/src/app/components/home/events-pages/movies-page/movies-page.component.ts
+++ TicketsFront/src/app/components/home/events-pages/movies-page/movies-page.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import {Concert} from '../../../../entities/Concert';
+import {HttpClient} from '@angular/common/http';
+import {LinkService} from '../../../../services/link.service';
+import {Router} from '@angular/router';
+import {Movie} from '../../../../entities/Movie';
+import {HeaderComponent} from '../../../header/header.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-movies-page',
-  imports: [],
+    imports: [
+        HeaderComponent,
+        NgIf
+    ],
   templateUrl: './movies-page.component.html',
   styleUrl: './movies-page.component.css'
 })
 export class MoviesPageComponent {
+    movies: Movie[];
 
+    constructor(private http: HttpClient, protected link: LinkService, private router: Router) {}
+
+    ngOnInit()
+    {
+        this.http.get(this.link.url+"/movies/getAll").subscribe(
+            (response: any)=>{
+                this.movies=response;
+            }
+        );
+    }
+
+    openMovie(id: string)
+    {
+        this.router.navigateByUrl("/movies/"+id);
+    }
 }

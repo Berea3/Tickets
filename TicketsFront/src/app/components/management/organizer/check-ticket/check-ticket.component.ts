7acf4f3b6f008480fca7ca6f5e72be7808f607e2
Berendea Andrei <berendeaandrei03@gmail.com>
2025-05-31 23:00:25 +0300

    check ticket component

---
+++ TicketsFront/src/app/components/management/organizer/check-ticket/check-ticket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check-ticket',
+  imports: [],
+  templateUrl: './check-ticket.component.html',
+  styleUrl: './check-ticket.component.css'
+})
+export class CheckTicketComponent {
+
+}

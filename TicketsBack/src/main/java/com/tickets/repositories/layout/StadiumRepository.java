28c88070dcce7daff76e2072aef17df9df52d02e
Berendea Andrei <berendeaandrei03@gmail.com>
2025-06-09 11:13:01 +0300

    stadium repo

---
+++ TicketsBack/src/main/java/com/tickets/repositories/layout/StadiumRepository.java
@@ -0,0 +1,7 @@
+package com.tickets.repositories.layout;
+
+import com.tickets.entities.layouts.Stadium;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StadiumRepository extends JpaRepository<Stadium,String> {
+}

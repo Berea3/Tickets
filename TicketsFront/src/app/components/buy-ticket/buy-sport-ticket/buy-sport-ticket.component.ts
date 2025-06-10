17a4d1edc255fab3c4f9600638c3fe4af47e80d4
Berendea Andrei <berendeaandrei03@gmail.com>
2025-06-10 18:34:37 +0300

    buy sport ticket component

---
+++ TicketsFront/src/app/components/buy-ticket/buy-sport-ticket/buy-sport-ticket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buy-sport-ticket',
+  imports: [],
+  templateUrl: './buy-sport-ticket.component.html',
+  styleUrl: './buy-sport-ticket.component.css'
+})
+export class BuySportTicketComponent {
+
+}

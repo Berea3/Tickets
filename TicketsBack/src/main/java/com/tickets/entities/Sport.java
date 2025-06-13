65e20069cbf74fcda09d5b0ef70c9eb4a811f3f6
Berendea Andrei <berendeaandrei03@gmail.com>
2025-06-13 23:27:18 +0300

    sport entity

--- TicketsBack/src/main/java/com/tickets/entities/Sport.java
+++ TicketsBack/src/main/java/com/tickets/entities/Sport.java
@@ -1,8 +1,12 @@
 package com.tickets.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.tickets.entities.layouts.Stadium;
 import com.tickets.security.entities.User;
 import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Table(name="sports")
@@ -11,12 +15,111 @@ public class Sport {
     @Id
     private String id;
 
-    private String title;
+    private String name;
     private String description;
     private String type;
+    private LocalDate date;
+    private LocalTime time;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "poster_id", referencedColumnName = "id")
+    private Attachment poster;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "stadium_id", referencedColumnName = "id")
+    private Stadium stadium;
 
     @JsonIgnore
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name="user_id", referencedColumnName = "id")
     private User user;
+
+    public Sport() {}
+
+    public Sport(String id, String name, String description, String type, LocalDate date, LocalTime time, Attachment poster, Stadium stadium, User user) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.type = type;
+        this.date = date;
+        this.time = time;
+        this.poster = poster;
+        this.stadium = stadium;
+        this.user = user;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public Attachment getPoster() {
+        return poster;
+    }
+
+    public Stadium getStadium() {
+        return stadium;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public void setPoster(Attachment poster) {
+        this.poster = poster;
+    }
+
+    public void setStadium(Stadium stadium) {
+        this.stadium = stadium;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }

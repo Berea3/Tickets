ab3858935674c288b36bf6c08c64456f7eb438dc
Berendea Andrei <berendeaandrei03@gmail.com>
2025-06-29 18:59:24 +0300

    dtos

--- TicketsBack/src/main/java/com/tickets/dto/TicketDto.java
+++ TicketsBack/src/main/java/com/tickets/dto/TicketDto.java
@@ -5,10 +5,18 @@ public class TicketDto {
     public String id;
     public String event;
     public String eventName;
+    public String description;
 
     public TicketDto(String id, String event, String eventName) {
         this.id = id;
         this.event = event;
         this.eventName = eventName;
+    }
+
+    public TicketDto(String id, String event, String eventName, String description) {
+        this.id = id;
+        this.event = event;
+        this.eventName = eventName;
+        this.description = description;
     }
 }

eab0140dec33ae1bb50f1653d0714f644bb8d2a0
Berendea Andrei <berendeaandrei03@gmail.com>
2025-06-30 10:34:20 +0300

    home component

--- TicketsFront/src/app/components/home/home.component.ts
+++ TicketsFront/src/app/components/home/home.component.ts
@@ -4,40 +4,69 @@ import { HttpClient } from "@angular/common/http";
 import {LinkService} from "../../services/link.service";
 import {Theater} from "../../entities/Theater";
 import {NgForOf, NgIf} from "@angular/common";
-import {Router} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {BearBtnComponent, BearInputCheckboxComponent, BearInputTextComponent} from 'bear-library';
+import {EventDto} from '../../dtos/EventDto';
 
 @Component({
   selector: 'app-home',
   standalone: true,
     imports: [
         HeaderComponent,
-        NgIf,
+        BearInputCheckboxComponent,
+        BearInputTextComponent,
     ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
     theaters: Theater[];    // will change to event
+    events: EventDto[];
+    filteredEvents: EventDto[];
+    showOnlyAvailable: boolean=false;
+    searchText: string="";
 
     constructor(private http: HttpClient, protected link: LinkService, private router: Router) {}
 
     ngOnInit()
     {
-        this.http.get(this.link.url+"/theaters/getAll").subscribe(
-            (data: any)=>{
-                this.theaters=data;
-                console.log(data);
+        this.http.get(this.link.url+"/events/getAll").subscribe(
+            (response: any)=>{
+                this.events=response;
+                this.filteredEvents=response;
             }
-        )
+        );
+        // this.http.get(this.link.url+"/theaters/getAll").subscribe(
+        //     (data: any)=>{
+        //         this.theaters=data;
+        //         console.log(data);
+        //     }
+        // )
     }
 
-    openTheatre(id: string)
+    goToBuyTicket(event: EventDto)
     {
-        this.router.navigateByUrl("/theater/"+id);
+        this.router.navigateByUrl("/buy/"+event.eventType+"/"+event.id);
     }
 
-    goToBuyTicket(id: string)
+    setExpired(showOnlyAvailable: boolean)
     {
-        this.router.navigateByUrl("/buy/theater/"+id);
+        this.showOnlyAvailable=showOnlyAvailable;
+        this.filterEvents();
+    }
+
+    changeSearch(text: string)
+    {
+        this.searchText=text;
+        this.filterEvents();
+    }
+
+    filterEvents()
+    {
+        console.log(new Date());
+        console.log(this.searchText);
+        this.filteredEvents=this.events.filter((event)=>event.name.toLowerCase().includes(this.searchText.toLowerCase()) || event.eventType.toLowerCase().includes(this.searchText.toLowerCase()));   //|| event.description.toLowerCase().includes(this.searchText.toLowerCase())
+        if (this.showOnlyAvailable) this.filteredEvents=this.filteredEvents.filter((event)=>new Date(event.date).getTime()>new Date().getTime());
+        console.log(this.filteredEvents);
     }
 }

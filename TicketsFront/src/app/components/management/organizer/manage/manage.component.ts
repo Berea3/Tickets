9fd695f6bbd5563de7abd81d54c6d427b3a4a639
Berendea Andrei <berendeaandrei03@gmail.com>
2025-06-30 10:34:32 +0300

    manage component

--- TicketsFront/src/app/components/management/organizer/manage/manage.component.ts
+++ TicketsFront/src/app/components/management/organizer/manage/manage.component.ts
@@ -3,13 +3,17 @@ import {HeaderComponent} from '../../../header/header.component';
 import {BearBtnComponent, BearInputCheckboxComponent} from 'bear-library';
 import {NgForOf, NgIf} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {LinkService} from '../../../../services/link.service';
+import {EventDto} from '../../../../dtos/EventDto';
+import {PlaceDto} from '../../../../dtos/PlaceDto';
+import {resolve} from '@angular/compiler-cli';
 
 @Component({
   selector: 'app-manage',
     imports: [
         HeaderComponent,
         BearBtnComponent,
-        BearInputCheckboxComponent,
         NgIf,
         NgForOf,
         RouterLink
@@ -19,16 +23,47 @@ import {RouterLink} from '@angular/router';
 })
 export class ManageComponent {
     selectedTab: 'events' | 'places' = 'events';
+    events: EventDto[];
+    places: PlaceDto[];
 
-    events = [
-        { title: 'Music Concert', description: 'Live performance at the arena.' },
-        { title: 'Tech Meetup', description: 'Discuss the latest in web dev.' },
-        { title: 'Art Exhibition', description: 'Modern art gallery showing.' }
-    ];
+    constructor(private http: HttpClient, private link: LinkService) {}
 
-    places = [
-        { name: 'Central Park', location: 'Downtown City' },
-        { name: 'Oceanview Café', location: 'Seaside Road' },
-        { name: 'Mountain Retreat', location: 'North Highlands' }
-    ];
+    // events = [
+    //     { title: 'Music Concert', description: 'Live performance at the arena.' },
+    //     { title: 'Tech Meetup', description: 'Discuss the latest in web dev.' },
+    //     { title: 'Art Exhibition', description: 'Modern art gallery showing.' }
+    // ];
+
+    // places = [
+    //     { name: 'Central Park', location: 'Downtown City' },
+    //     { name: 'Oceanview Café', location: 'Seaside Road' },
+    //     { name: 'Mountain Retreat', location: 'North Highlands' }
+    // ];
+
+    ngOnInit()
+    {
+        this.http.get(this.link.url+"/organizer/getEvents").subscribe(
+            (response: any)=>{
+                this.events=response;
+            }
+        );
+        this.http.get(this.link.url+"/organizer/getPlaces").subscribe(
+            (response: any)=>{
+                this.places=response;
+            }
+        );
+    }
+
+    deletePlace(place: PlaceDto)
+    {
+        this.http.delete(this.link.url+"/"+place.placeType+"/delete/"+place.id).subscribe(
+            (response: any)=>{
+                this.http.get(this.link.url+"/organizer/getPlaces").subscribe(
+                    (response: any)=>{
+                        this.places=response;
+                    }
+                );
+            }
+        )
+    }
 }
